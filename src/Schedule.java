229f389d1982a2f8deadc23de3439a6205100edc
nalam <nalam@svsu.edu>
2019-10-16 14:24:04 -0400

    cleaned plane code

--- src/Schedule.java
+++ src/Schedule.java
@@ -1,55 +1,67 @@
+import java.sql.*;
+
 
 public class Schedule {
-	public Schedule(int tu_id, int cus_tuid, int flight_tuid, String flight_date, String seat_type, int seat_no) {
-		super();
-		this.tu_id = tu_id;
-		this.cus_tuid = cus_tuid;
-		this.flight_tuid = flight_tuid;
-		this.flight_date = flight_date;
-		this.seat_type = seat_type;
-		this.seat_no = seat_no;
+	public static void main(String[] args) {
+
+		try {
+
+			Connection connection;
+			connection = DriverManager.getConnection("jdbc:sqlite:C:\\Users\\nalam\\IdeaProjects\\Plane_Scheduler\\plane.db");
+
+			//for fetching the data
+			String query3 = "SELECT * FROM Seating ";
+			Statement preparedStatement3 = connection.createStatement();
+			ResultSet rs = preparedStatement3.executeQuery(query3);
+
+
+			//for inserting the data
+			String queryIn = "INSERT INTO Schedule  VALUES(?, ?, ?, ?, ?, ?)";
+			PreparedStatement preparedStatement1 = connection.prepareStatement(queryIn);
+
+			int i = 1;
+
+			boolean seat_ase = true;
+			// iterate through the java resultset
+			while (rs.next()) {
+				int cus_tuid = rs.getInt("Cus_tuid");
+				int plane_tuid = rs.getInt("Plane_tuid");
+				String seat_type = rs.getString("Seat_type");
+				String date = rs.getString("Date");
+				System.out.format("%s %s %s %s\n", cus_tuid, plane_tuid, seat_type, date);
+
+				preparedStatement1.setInt(1, i);
+				preparedStatement1.setInt(2, cus_tuid);
+				preparedStatement1.setString(4, seat_type);
+
+				if(seat_ase) {
+					preparedStatement1.setInt(3, plane_tuid);
+					preparedStatement1.setString(5, date);
+					preparedStatement1.setInt(6, i);
+
+				}
+
+
+				preparedStatement1.executeUpdate();
+				i++;
+			}
+			preparedStatement3.close();
+
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+
+
+
+
 	}
-	private int tu_id;
-	private int cus_tuid;
-	private int flight_tuid;
-	private String flight_date;
-	private	String seat_type;
-	private int seat_no;
-	public int getTu_id() {
-		return tu_id;
-	}
-	public void setTu_id(int tu_id) {
-		this.tu_id = tu_id;
-	}
-	public int getFlight_tuid() {
-		return flight_tuid;
-	}
-	public void setFlight_tuid(int flight_tuid) {
-		this.flight_tuid = flight_tuid;
-	}
-	public int getCus_tuid() {
-		return cus_tuid;
-	}
-	public void setCus_tuid(int cus_tuid) {
-		this.cus_tuid = cus_tuid;
-	}
-	public String getFlight_date() {
-		return flight_date;
-	}
-	public void setFlight_date(String flight_date) {
-		this.flight_date = flight_date;
-	}
-	public String getSeat_type() {
-		return seat_type;
-	}
-	public void setSeat_type(String seat_type) {
-		this.seat_type = seat_type;
-	}
-	public int getSeat_no() {
-		return seat_no;
-	}
-	public void setSeat_no(int seat_no) {
-		this.seat_no = seat_no;
-	}
-	
+
+
+
+
+
+
 }
