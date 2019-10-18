b5ef39397e1d867a6961990cf88ee9a52022e487
Noor Alam <nalam@svsu.edu>
2019-10-18 01:14:05 -0400

    sql

--- src/Schedule.java
+++ src/Schedule.java
@@ -19,334 +19,93 @@ public class Schedule {
 			connection = DriverManager.getConnection("jdbc:sqlite:C:\\Users\\nalam\\IdeaProjects\\Plane_Scheduler\\plane.db");
 
 			//for fetching the data
-			String query3 = "Select Max(TUID), Count(Cus_tuid),Cus_tuid , date, Plane_tuid, seat_type" +
-                    " From Seating " +
-                    "Group by Date,Plane_tuid " +
-                    "Order by Date,Plane_tuid, seat_type desc ";
+            String seating_out = "SELECT * FROM Seating ";
 
-			Statement preparedStatement3 = connection.createStatement();
+			Statement seating_select = connection.createStatement();
 
-			ResultSet rs = preparedStatement3.executeQuery(query3);
+			ResultSet seating_rs = seating_select.executeQuery(seating_out);
 
 
-
+            int index =1;
 
             System.out.println();
 
 			//for inserting the data
-        	String queryIn = "UPDATE Seating SET seat_type = ? WHERE Cus_tuid = ? ";
 
-    		PreparedStatement preparedStatement1 = connection.prepareStatement(queryIn);
-//
-//			int i = 1;a
-//
-//			int date1_planes [] = {1,1,1};
-//
-//			int date2_planes [] = {1,1,1};
-//
-//			int date3_planes [] = {1,1,1};
-//
-//
-//            HashMap<String, Integer> map = new HashMap<>();
+
+
+            String queryIn = "INSERT INTO Schedule  VALUES(?, ?, ?, ?, ?, ?)";
+            PreparedStatement schedule_insert = connection.prepareStatement(queryIn);
+
+
+
+
+
 
 
 			// iterate through the java resultset
-			while (rs.next()) {
+			while (seating_rs.next()) {
 
-                int tuid = rs.getInt("Max(TUID)");
-                int count = rs.getInt("Count(Cus_tuid)");
-				int cus_tuid = rs.getInt("Cus_tuid");
-				int plane_tuid = rs.getInt("Plane_tuid");
-				String seat_type = rs.getString("Seat_type");
-				String date = rs.getString("Date");
+			    // gets the seating data from the table
+                int cus_tuid = seating_rs.getInt("Cus_tuid");
+                int plane_tuid = seating_rs.getInt("Plane_tuid");
+                String seat_type = seating_rs.getString("Seat_type");
+                String date = seating_rs.getString("Date");
+
+                // gets available seats on the current plane
 
 
-                if(plane_tuid==1 & count >10) {
-                    if (seat_type.equals("V")) {
-                        preparedStatement1.setString(1, "L");
-			            preparedStatement1.setInt(2, cus_tuid);
+
+                if (seat_type.equals("V")) {
+
+                    int seat = available(date,plane_tuid);
+
+                    if(plane_tuid ==1 & seat <11 ) {
+
+                        schedule_insert.setInt(1, index);
+                        schedule_insert.setInt(2, cus_tuid);
+                        schedule_insert.setString(4, seat_type);
+                        schedule_insert.setInt(3, plane_tuid);
+                        schedule_insert.setString(5, date);
+                        schedule_insert.setInt(6, seat +1);
+
+                        add_seat(date,plane_tuid,seat);
 
                     }
 
-                }
-
-                preparedStatement1.executeUpdate();
-
-//				SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy");
+//                    seat = available(date,plane_tuid+1);
+//                    else if ( plane_tuid == 2  & ){
 //
-//				Calendar c = Calendar.getInstance();
+//                        schedule_insert.setInt(1, index);
+//                        schedule_insert.setInt(2, cus_tuid);
+//                        schedule_insert.setString(4, seat_type);
+//                        schedule_insert.setInt(3, plane_tuid);
+//                        schedule_insert.setString(5, date);
+//                        schedule_insert.setInt(6, seat +1);
 //
-//				c.setTime(sdf.parse(date));
-//
-//				String newDate = sdf.format(c.getTime());
-
-
-
-
-				System.out.format("%s %s %s %s %s %s\n", tuid, count,cus_tuid, date,  plane_tuid,seat_type);
-
-
-
-
-//				preparedStatement1.setInt(1, i);
-//				preparedStatement1.setInt(2, cus_tuid);
-//				preparedStatement1.setString(4, seat_type);
-
-
-				// checks if a vip
-//				if (seat_type.equals("V")) {
-//
-//                    if (date.equals(12/01/2020)) {
-//
-//                        //checks the plane
-//                        switch (plane_tuid) {
-//
-//                            //if passenger want plane 1
-//                            case 1:
-//                                //if plane 1 is not full books plane 1
-//                                if (date1_planes[0] < 11) {
-//                                    preparedStatement1.setInt(3, plane_tuid);
-//                                    preparedStatement1.setString(5, date);
-//                                    preparedStatement1.setInt(6, date1_planes[0]);
-//                                    date1_planes[0] += 1;
-//                                    // if plane 1 is full books plane 2
-//                                } else {
-//                                    preparedStatement1.setInt(3, plane_tuid + 1);
-//                                    preparedStatement1.setString(5, date);
-//                                    preparedStatement1.setInt(6, date1_planes[1]);
-//                                    date1_planes[1] += 1;
-//                                }
-//                                break;
-//                            case 0:
-//                                //if plane 1 is not full books plane 1
-//                                if (date1_planes[0] < 11) {
-//                                    preparedStatement1.setInt(3, plane_tuid+1);
-//                                    preparedStatement1.setString(5, date);
-//                                    preparedStatement1.setInt(6, date1_planes[0]);
-//                                    date1_planes[0] += 1;
-//                                    // if plane 1 is full books plane 2
-//                                } else {
-//                                    preparedStatement1.setInt(3, plane_tuid + 2);
-//                                    preparedStatement1.setString(5, date);
-//                                    preparedStatement1.setInt(6, date1_planes[1]);
-//                                    date1_planes[1] += 1;
-//                                }
-//                                break;
-//                            // if passenger wants plane 2
-//                            case 2:
-//
-//                                // if plane 2 is not full books it
-//                                if (date1_planes[1] < 8) {
-//                                    preparedStatement1.setInt(3, plane_tuid);
-//                                    preparedStatement1.setString(5, date);
-//                                    preparedStatement1.setInt(6, date1_planes[1]);
-//                                    date1_planes[1] += 1;
-//
-//                                    // if plane 2 is full goes to plane 3
-//                                } else  {
-//                                    preparedStatement1.setInt(3, plane_tuid + 1);
-//                                    preparedStatement1.setString(5, date);
-//                                    preparedStatement1.setInt(6, date1_planes[2]);
-//                                    date1_planes[2] += 1;
-//                                }
-//                                break;
-//                            // if passenger wants plane 3
-//                            case 3:
-//
-//                                // if plane 3 is not full books plane 3
-//                                if (date1_planes[2] < 14) {
-//                                    preparedStatement1.setInt(3, plane_tuid);
-//                                    preparedStatement1.setString(5, date);
-//                                    preparedStatement1.setInt(6, date1_planes[2]);
-//                                    date1_planes[2] += 1;
-//
-//                                    // if plane 3 is full goes to next day plane 1
-//                                } else  {
-//                                    preparedStatement1.setInt(3, plane_tuid-2);
-//                                    preparedStatement1.setString(5, "12/02/2020");
-//                                    preparedStatement1.setInt(6, date2_planes[0]);
-//                                    date2_planes[0] += 1;
-//                                }
-//                                break;
-//                        }
+//                        add_seat(date,plane_tuid,seat);
 //
 //                    }
-//
-//
-//
-//
-//			}
-//
-//
-//				if (seat_type.equals("L")) {
-//
-//					//checks teh date
-//					if (date.equals(12/01/2020)) {
-//
-//						//checks the plane
-//						switch (plane_tuid) {
-//
-//							//if passenger want plane 1
-//							case 1:
-//								//if plane 1 is not full books plane 1
-//								if (date1_planes[0] < 11) {
-//									preparedStatement1.setInt(3, plane_tuid);
-//									preparedStatement1.setString(5, date);
-//									preparedStatement1.setInt(6, date1_planes[0]);
-//									date1_planes[0] += 1;
-//									// if plane 1 is full books plane 2
-//								} else {
-//									preparedStatement1.setInt(3, plane_tuid + 1);
-//									preparedStatement1.setString(5, date);
-//									preparedStatement1.setInt(6, date1_planes[1]);
-//									date1_planes[1] += 1;
-//								}
-//								break;
-//							case 0:
-//								//if plane 1 is not full books plane 1
-//								if (date1_planes[0] < 11) {
-//									preparedStatement1.setInt(3, plane_tuid+1);
-//									preparedStatement1.setString(5, date);
-//									preparedStatement1.setInt(6, date1_planes[0]);
-//									date1_planes[0] += 1;
-//									// if plane 1 is full books plane 2
-//								} else {
-//									preparedStatement1.setInt(3, plane_tuid + 2);
-//									preparedStatement1.setString(5, date);
-//									preparedStatement1.setInt(6, date1_planes[1]);
-//									date1_planes[1] += 1;
-//								}
-//								break;
-//							// if passenger wants plane 2
-//							case 2:
-//
-//								// if plane 2 is not full books it
-//								if (date1_planes[1] < 8) {
-//									preparedStatement1.setInt(3, plane_tuid);
-//									preparedStatement1.setString(5, date);
-//									preparedStatement1.setInt(6, date1_planes[1]);
-//									date1_planes[1] += 1;
-//
-//									// if plane 2 is full goes to plane 3
-//								} else  {
-//									preparedStatement1.setInt(3, plane_tuid + 1);
-//									preparedStatement1.setString(5, date);
-//									preparedStatement1.setInt(6, date1_planes[2]);
-//									date1_planes[2] += 1;
-//								}
-//								break;
-//							// if passenger wants plane 3
-//							case 3:
-//
-//								// if plane 3 is not full books plane 3
-//								if (date1_planes[2] < 14) {
-//									preparedStatement1.setInt(3, plane_tuid);
-//									preparedStatement1.setString(5, date);
-//									preparedStatement1.setInt(6, date1_planes[2]);
-//									date1_planes[2] += 1;
-//
-//									// if plane 3 is full goes to next day plane 1
-//								} else  {
-//									preparedStatement1.setInt(3, plane_tuid-2);
-//									preparedStatement1.setString(5, "12/02/2020");
-//									preparedStatement1.setInt(6, date2_planes[0]);
-//									date2_planes[0] += 1;
-//								}
-//								break;
-//						}
-//
-//					}
-//
-//					//if passenger wants date 2
-//
-//					else if (date.equals("12/02/2020")) {
-//
-//						switch (plane_tuid) {
-//
-//							//if passenger wants plane 1
-//							case 1:
-//
-//								// if plane 1 is not full books plane 1
-//								if (date2_planes[0] < 10) {
-//									preparedStatement1.setInt(3, plane_tuid);
-//									preparedStatement1.setString(5, date);
-//									preparedStatement1.setInt(6, date2_planes[0]);
-//									date2_planes[0] += 1;
-//
-//									// if plane 1 is full goes to plane 2
-//								} else  {
-//									preparedStatement1.setInt(3, plane_tuid+1);
-//									preparedStatement1.setString(5, date);
-//									preparedStatement1.setInt(6, date2_planes[1]);
-//									date2_planes[1] += 1;
-//
-//								}
-//								break;
-//
-//							case 0:
-//								//if plane 1 is not full books plane 1
-//								if (date1_planes[0] < 11) {
-//									preparedStatement1.setInt(3, plane_tuid);
-//									preparedStatement1.setString(5, date);
-//									preparedStatement1.setInt(6, date1_planes[0]);
-//									date1_planes[0] += 1;
-//									// if plane 1 is full books plane 2
-//								} else {
-//									preparedStatement1.setInt(3, plane_tuid + 1);
-//									preparedStatement1.setString(5, date);
-//									preparedStatement1.setInt(6, date1_planes[1]);
-//									date1_planes[1] += 1;
-//								}
-//								break;
-//							// if passenger wants plane 2
-//							case 2:
-//								// if plane 2 is not full
-//								if (date2_planes[1] < 8) {
-//									preparedStatement1.setInt(3, plane_tuid);
-//									preparedStatement1.setString(5, date);
-//									preparedStatement1.setInt(6, date2_planes[1]);
-//									date2_planes[1] += 1;
-//
-//									// if plane 2 is full goes to plane 3
-//								} else  {
-//									preparedStatement1.setInt(3, plane_tuid+1);
-//									preparedStatement1.setString(5, date);
-//									preparedStatement1.setInt(6, date2_planes[2]);
-//									date2_planes[2] += 1;
-//
-//								}
-//								break;
-//							// if passenger wants plane 3
-//
-//							case 3:
-//								// if plane 3 is not full
-//								if (date2_planes[2] < 14) {
-//									preparedStatement1.setInt(3, plane_tuid);
-//									preparedStatement1.setString(5, date);
-//									preparedStatement1.setInt(6, date2_planes[2]);
-//									date2_planes[2] += 1;
-//								}
-//
-//								else  {
-//									preparedStatement1.setInt(3, plane_tuid - 2);
-//									preparedStatement1.setString(5, "12/03/2020");
-//									preparedStatement1.setInt(6, date3_planes[0]);
-//									date3_planes[0] += 1;
-//								}
-//								break;
-//						}
-//					}
-//
-//
-//
-//				}
 
 
-				//preparedStatement1.executeUpdate();
-				//i++;
+
+
+                }
+
+                schedule_insert.executeUpdate();
+
+
+
+
+				//System.out.format("%s %s %s %s %s %s\n", tuid, count,cus_tuid, date,  plane_tuid,seat_type);
+
+
+
+
+
 			}
 
-			preparedStatement3.close();
+			seating_select.close();
 
 //			System.out.println(Arrays.toString(date1_planes));
 //			System.out.println(Arrays.toString(date2_planes));
@@ -364,8 +123,41 @@ public class Schedule {
 	}
 
 
+	 static int available(String date,int plane_tuid ) throws SQLException {
+        // gets available seats on the current plane
+        Connection connection;
+        connection = DriverManager.getConnection("jdbc:sqlite:C:\\Users\\nalam\\IdeaProjects\\Plane_Scheduler\\plane.db");
+
+        String avail_out = "Select * From Date_time WHERE Date = ? AND WHERE plane_id = ? ";
+         ResultSet avail_rs;
+         try (PreparedStatement date_time = connection.prepareStatement(avail_out)) {
+
+             date_time.setString(1, date);
+             date_time.setInt(2, plane_tuid);
+
+             avail_rs = date_time.executeQuery(avail_out);
+         }
 
 
+         int recent_seat = avail_rs.getInt("seat_taken");
 
 
+         return recent_seat;
+
+    }
+
+
+    static  void add_seat (String date,int plane_tuid, int recent_seat ) throws SQLException {
+        // gets available seats on the current plane
+        Connection connection;
+        connection = DriverManager.getConnection("jdbc:sqlite:C:\\Users\\nalam\\IdeaProjects\\Plane_Scheduler\\plane.db");
+
+        String avail_in = "UPDATE Date_tim SET seat_taken = ? WHERE Date = ? AND WHERE plane_id = ? ";
+        PreparedStatement avail_prepare = connection.prepareStatement(avail_in);
+
+        avail_prepare.setInt(1,recent_seat+1);
+        avail_prepare.setString(2,date);
+        avail_prepare.setInt(3,plane_tuid);
+        avail_prepare.executeUpdate();
+    }
 }
